cb15d17dce6bda4c2799b9e52e843982fa9c958b
Matteo Borgesano <matteoborgesano@computer-3.station>
2020-12-10 19:27:23 +0100

    Svolti esercizi per 11 dicembre

--- esercizi04122020.js
+++ esercizi04122020.js
@@ -1,3 +1,25 @@
+"Use strict"
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let inserisciPari = () => {
+    let num 
+    do {
+        num = prompt("Inserisci numero pari")
+    } while ((num%2) !== 0)
+    return num
+}
+function maggiore(a, b){
+    if (a>b){
+        console.log("Il valore maggiore tra "+a+" e "+b+" è "+a)
+        return a
+    } else {
+        console.log("Il valore maggiore tra "+a+" e "+b+" è "+b)
+        return b
+    }
+}
 function inserisciIntero(stringa, min, max){
     let a = parseInt(min)
     let b = parseInt(max)
@@ -8,29 +30,67 @@ function inserisciIntero(stringa, min, max){
     console.log("Inserito "+c+" con successo")
     return parseInt(c)
 }
+function divisori(MIN, MAX){
+    let k = inserisciIntero("K", MIN, MAX)
+    let lista_divisori = []
+    for (let i = k; i>0; i--){
+        if ((k%i)===0){
+            lista_divisori.push(i)
+        }
+    }
+    alert(lista_divisori)
+    return lista_divisori
+}
 
-let N = inserisciIntero("N", 0, +Infinity)
-let min = inserisciIntero("minimo", -Infinity, +Infinity)
-let max = inserisciIntero("massimo", min, +Infinity)
-let valori = []
-let pari = 0
-let m = (min+max)/2
-let somma = 0
-let n = 0
-for (let i = 0; i<N; i++){
-    valori.push(inserisciIntero("numero", min, max))
-}
-for (let i = 0; i<valori.length; i++){
-    if ((valori[i]%2)===0){
-        pari++
+
+function isPrimo(MIN, MAX){
+    Divisori = divisori(MIN, MAX)
+    if (Divisori.lenght === 2 && divisori[1] === 1){
+        return true
+    } else {
+        return false
     }
 }
-alert("Ci sono "+pari+" numeri pari")
-for (let i = 0; i<valori.length; i++){
-    if (valori[i]>m){
-        somma += valori[i]
-        n++        
+
+function moda(){
+    let voti = []
+    let Moda = 0
+    let temp
+    let n 
+    //0 c'è sempre una sola volta da definizione
+    voti.push(1)
+    for (let i = 1; i<10; i++){
+        voti[i] = 0
     }
+    do {
+        n = inserisciIntero("Inserisci il voto numero", 0, 10)
+        voti[n-1] = voti[n-1]+1
+    } while (n!==0)
+    temp = voti[0]
+    for (let i = 1; i<voti.length;i++){
+        if (temp<voti[i]){
+            temp = voti[i]
+            Moda = i+1
+        }
+    }
+    return Moda
+    
 }
-let media = somma/n
-alert("La media dei valori maggiori di "+m+" è "+media)
+function indovinaNumero(){
+    let num = getRandomInt(0, 1000)
+    console.log(num)
+    let numero
+    let loop = true
+    do {
+        numero = +prompt("inserisci numero")
+        if (numero<num){
+            alert("Il numero coretto è maggiore")
+        } 
+        else if (numero>num){
+            alert("Il numero corretto è minore")
+        } else {
+            alert("Hai indovinato!")
+            loop = false
+        }
+    } while (loop === true)
+}
